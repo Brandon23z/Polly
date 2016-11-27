d6d6a2684132e5e4783092802e2b8dd73a0c7174
adpapaio <adpapaio@umich.edu>
2016-11-27 17:12:06 -0500

    Slope offset set to 90

--- Assets/Scripts/CharcaterMove.cs
+++ Assets/Scripts/CharcaterMove.cs
@@ -3,22 +3,28 @@ using System.Collections;
 
 public class CharcaterMove : MonoBehaviour {
 
-		public float speed = 15.7F;
-		public float jumpSpeed = 15.0F;
+	public float speed = 15.7F;
+	public float jumpSpeed = 15.0F;
 	public float gravity = 20.0F;
-		private Vector3 moveDirection = Vector3.zero;
-		void Update() {
-			CharacterController controller = GetComponent<CharacterController>();
-			if (controller.isGrounded) {
-				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-				moveDirection = transform.TransformDirection(moveDirection);
-				moveDirection *= speed;
-				if (Input.GetButton("Jump"))
-					moveDirection.y = jumpSpeed;
+	private Vector3 moveDirection = Vector3.zero;
 
-			}
-			moveDirection.y -= gravity * Time.deltaTime;
-			controller.Move(moveDirection * Time.deltaTime);
+	void Update()
+	{
+		CharacterController controller = GetComponent<CharacterController>();
+
+
+		if (controller.isGrounded)
+		{
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+		
 		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
 
 }
