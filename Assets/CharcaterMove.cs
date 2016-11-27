79a8ea78a56d1aeda9611db4d82d8dfa2b51c640
adpapaio <adpapaio@umich.edu>
2016-11-27 13:34:00 -0500

    Added Character Movement, MetaData

---
+++ Assets/CharcaterMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharcaterMove : MonoBehaviour {
+
+		public float speed = 15.7F;
+		public float jumpSpeed = 15.0F;
+	public float gravity = 20.0F;
+		private Vector3 moveDirection = Vector3.zero;
+		void Update() {
+			CharacterController controller = GetComponent<CharacterController>();
+			if (controller.isGrounded) {
+				moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= speed;
+				if (Input.GetButton("Jump"))
+					moveDirection.y = jumpSpeed;
+
+			}
+			moveDirection.y -= gravity * Time.deltaTime;
+			controller.Move(moveDirection * Time.deltaTime);
+		}
+
+}
