6c9aa6f72e1596ce47cf57e0eabb08ed0be5a2fa
adpapaio <adpapaio@umich.edu>
2016-11-30 02:05:09 -0500

    Started the Healthbar code

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class HealthBar : MonoBehaviour {
 
+	private Image ima;
+	public Sprite someOtherSprite;
+	public int health;
+	public int maxHealth;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		health = 10;
+		maxHealth = 10;
+		ima = this.GetComponent<Image> ();
 	}
-	
+
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.H))
+		{
+			health--;
+			LoadSprite ();
+		}
+		if (Input.GetKeyDown (KeyCode.J))
+		{
+			health = maxHealth;
+			LoadSprite ();
+		}
+
+	}
+
+	public void LoadSprite ()
+	{
+		if (health == 9) 
+		{
+			someOtherSprite = Resources.Load<Sprite> ("9Health");
+
+			ima.sprite = someOtherSprite;
+		}
 	}
 }
