ac038ee693108aae54dba22c967f2ac439a8092a
adpapaio <adpapaio@umich.edu>
2016-12-01 21:10:09 -0500

    Added Healthbar Prefab, Non-Functional Healthbar,Fixed UI
    There is a healthbar script its all setup but it is not detecting
    collisons correctly

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	RectTransform healthbar;
+	public Canvas Health;
+	public float hitValue = .2f;
+	public float defaultHeight = .25f;
+	public float defaultWidth = 2f;
+	public float currHealth;
+	// Use this for initialization
+	void Start ()
+	{
+		healthbar = Health.GetComponentInChildren<RectTransform> ();
+
+		currHealth = 2f;
+	}
+
+
+
+	void OnColliderEnter (Collider other) 
+	{
+		if (other.gameObject.tag == "Enemy")
+		{
+			healthbar.sizeDelta = new Vector2 ((currHealth-hitValue), defaultHeight);
+			currHealth = currHealth - hitValue;
+		}
+	}
+
+
+}
