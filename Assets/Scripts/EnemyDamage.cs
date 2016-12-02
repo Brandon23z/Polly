61905b8c411c468e47fb48e93b83bc0220e08afd
Brandon23z <brandonmanni@gmail.com>
2016-12-02 18:53:50 -0500

    Added new Health Bar. Sped up Enemies.
    Now the game is actually fun and a challenge.

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamage : MonoBehaviour {
+
+	public float damage = 50;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			other.SendMessage("TakeDamage", damage);
+		}
+	}
+}
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class HealthBar : MonoBehaviour {
 
-	RectTransform healthbar;
+	/*RectTransform healthbar;
 	public Canvas Health;
 	public float hitValue = .2f;
 	public float defaultHeight = .25f;
@@ -20,14 +20,42 @@ public class HealthBar : MonoBehaviour {
 
 
 
-	void OnColliderEnter (Collider other) 
+	void OnTriggerEnter (Collider other) 
 	{
 		if (other.gameObject.tag == "Enemy")
 		{
 			healthbar.sizeDelta = new Vector2 ((currHealth-hitValue), defaultHeight);
 			currHealth = currHealth - hitValue;
 		}
+	}*/
+	
+	public Image CurrentHealthBar;
+	
+	public float currentHealth = 100;
+	private float maxHealth = 100;
+	
+	private void Start()
+	{
+		UpdateHealthBar();
 	}
-
-
+	
+	private void UpdateHealthBar()
+	{
+		float ratio = currentHealth/maxHealth;
+		CurrentHealthBar.rectTransform.localScale = new Vector3(ratio,1,1);
+	}
+	
+	private void TakeDamage(float damage)
+	{
+		currentHealth -= damage;
+		if(currentHealth < 0)
+		{
+			currentHealth = 0;
+		}
+		if(currentHealth > maxHealth)
+		{
+			currentHealth = maxHealth;
+		}
+		UpdateHealthBar();
+	}
 }
