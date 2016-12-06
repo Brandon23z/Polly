f31718cf75a8d6e8e356d128c32613c87a179a59
adpapaio <adpapaio@umich.edu>
2016-12-06 17:02:58 -0500

    Added Main Menu, Start Button, Quit Button, Credits
    Also Nonfunctional Options Button as well as a Opening animation for the
    main menu

---
+++ Assets/QuitGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+	public void QuitClicked(int clicked)
+	{
+		if (clicked == 1)
+			Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoad : MonoBehaviour
+{
+
+	public void LoadByIndex(int sceneDex)
+	{
+		SceneManager.LoadScene (sceneDex);
+
+	}
+}
